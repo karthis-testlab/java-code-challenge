27cbf4c4e1d60b815a7f2d4bba1cf6fb04d509b7
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-12-24 15:30:16 +0530

    Practice Date: 24/12/2023:03:30PM

---
+++ src/com/integer/challenge/RemoveMatchingElement.java
@@ -0,0 +1,71 @@
+package com.integer.challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveMatchingElement {
+	
+	/*
+	 * Given the integer array and the target, 
+	 * remove the matching target from the array 
+	 * 
+	 */
+	
+	@Test
+	public void postive_testcase() {
+		int[] nums = {1,2,3,4,99,5};
+		int target = 99;
+		Assert.assertArrayEquals(new int[] {1,2,3,4,5}, removeMatchingElement(nums, target));
+	}	
+
+	@Test
+	public void edge_testcase() {
+		int[] case1 = {1,2,3,4,5,99};
+		int target = 99;
+		Assert.assertArrayEquals(new int[] {1,2,3,4,5}, removeMatchingElement(case1, target));
+		int[] case2 = {1,2,3,99,4,99,5,99};
+		Assert.assertArrayEquals(new int[] {1,2,3,4,5}, removeMatchingElement(case2, target));
+	}
+	
+	@Test
+	public void negative_testcase() {
+		int[] nums = {1,2,3,4,5};
+		int target = 99;
+		Assert.assertArrayEquals(nums, removeMatchingElement(nums, target));
+	}
+	
+	/*
+	 * Brute force !!
+	 * Pseudo Code here:
+	 *
+	 * 1) Create one array list object
+	 * 2) Take the input array
+	 * 3) Traverse every element using for loop
+	 * 4) Compare the each integer element with the target number
+	 * 5) Push the all not matching element into the list object
+	 * 6) Create one integer array with size equal to the list object size
+	 * 7) Add all the integer from list to newly created integer array using for loop
+	 * 8) Finally, return the newly created integer array !!
+	 * 
+	 * 
+	 */
+	
+	private int[] removeMatchingElement(int[] nums, int target) {	
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != target) {
+				list.add(nums[i]);
+			}
+		}
+		
+		int[] newNumArray = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			newNumArray[i] = list.get(i);
+		}
+		return newNumArray;
+	}
+
+}
