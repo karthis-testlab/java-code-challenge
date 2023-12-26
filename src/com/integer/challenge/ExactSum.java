c9325771b32b6853f664e21682e869f8e389093f
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-12-26 23:30:28 +0530

    26/12/2023:11:30:00PM

---
+++ src/com/integer/challenge/ExactSum.java
@@ -0,0 +1,35 @@
+package com.integer.challenge;
+
+import org.junit.Test;
+
+public class ExactSum {
+	
+	@Test
+	public void example1() {
+		int[] nums = {1,7,4,3,1,2,1,5,1};
+		int k = 7;
+		findExactSumIndicies(nums, k);
+	}
+
+	/*
+	 * 1) Start with first element
+	 * 2) If the sum is less than the desired sum, slide to next element [Grow]
+	 * 3) Again, sum that and check if it is lesser, equal or greater
+	 * 4) If it is lesser, add the next element to slide [Grow]
+	 * 5) If it is greater, than shrink the last element on the left [Shrink]
+	 * 6) If it is equal, you got it and again do both Grow and Shrink [slide]
+	 */
+	private void findExactSumIndicies(int[] nums, int k) {
+		int left = 0, sum = 0;
+		for (int right = 0; right < nums.length; right++) { // right always traverse.
+			sum += nums[right];
+			while(sum > k) {
+				sum -= nums[left++];
+			}
+			if(sum == k) {
+				System.out.println(left+","+right);
+			}
+		}
+	}
+
+}
---
+++ src/com/integer/challenge/FindMaxSum.java
@@ -0,0 +1,37 @@
+package com.integer.challenge;
+
+import org.junit.Test;
+
+public class FindMaxSum {
+	
+	@Test
+	public void example1() {
+		int[] nums = {1,5,2,3,7,1};
+		int k = 3;
+		findMaxSum(nums, k);
+	}
+	
+	
+	/*
+	 * 1) Add first k elements and have a sum
+	 * 2) loop from k element until end 
+	 *    a) subtract the first element from the sum
+	 *    b) add the new element to the sum
+	 *    c) compare with existing sum --> whichever max --> make it max 
+	 */
+	private void findMaxSum(int[] nums, int k) {
+		int maxSum = 0, windowSum = 0;
+		for (int i = 0; i < k; i++) {
+			windowSum += nums[i];
+		}
+		
+		maxSum = windowSum;
+		
+		for (int i = k; i < nums.length; i++) {
+			windowSum = windowSum - nums[i-k] + nums[i];
+			maxSum = Math.max(maxSum, windowSum);
+		}
+		System.out.println(maxSum);
+	}
+
+}
