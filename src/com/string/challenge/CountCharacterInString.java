b633dccf6484f77c4db66e77aa0a019fd1ff84ec
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-12-29 23:50:58 +0530

    29/12/2023:11:50:00PM

--- src/com/string/challenge/CountCharacterInString.java
+++ src/com/string/challenge/CountCharacterInString.java
@@ -9,11 +9,14 @@ public class CountCharacterInString {
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
 		String str = "Karthikeyan";
 		char[] charArray = str.toCharArray();
-		for (int i = 0; i < charArray.length; i++) {
+		for (int i = 0; i < charArray.length; i++) {			
 			if (map.containsKey(charArray[i])) {
+				System.out.println(charArray[i]);
 				map.put(charArray[i], map.get(charArray[i]) + 1);
+			} else {
+				map.put(charArray[i], 1);
 			}
-			map.put(charArray[i], 1);
+			
 		}
 		System.out.println(map);
 	}
