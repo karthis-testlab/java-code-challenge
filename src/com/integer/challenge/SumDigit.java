6f4d0b06b809bfea7dd282fd3619d6bc25279a3b
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-02 13:56:26 +0530

    02/01/2024:01:56:00PM

--- src/com/integer/challenge/SumDigit.java
+++ src/com/integer/challenge/SumDigit.java
@@ -7,22 +7,43 @@ public class SumDigit {
 	@Test
 	public void testSumDigit() {
 		
-		int number = 9;
-		System.out.println(findSumDigit(number));
+		int number = 123456;
+		System.out.println(findSumDigitUsingWhileLoop(number));
+		System.out.println(findSumDigitUsingForLoop(number));
+		System.out.println(number/10);
+		System.out.println(number%10);
 
 	}
 
-	int findSumDigit(int number) {
+	/*
+	 * 1) 
+	 * 
+	 * 
+	 */
+	int findSumDigitUsingWhileLoop(int number) {
 		
-		int sum = 0, digit = 0;
+		int sum = 0;
 		
 		if(number%10 == 0)
 			return number;
 		
 		while (number > 0) {
-			digit = number%10;
-			sum += digit;
+			sum += number%10;
 			number /= 10;
+		}
+		
+		return sum;
+	}
+	
+	int findSumDigitUsingForLoop(int number) {
+		
+		int sum;
+		
+		if(number%10 == 0)
+			return number;
+		
+		for (sum = 0; number != 0; number/=10) {
+			sum+=number%10;
 		}
 		
 		return sum;
