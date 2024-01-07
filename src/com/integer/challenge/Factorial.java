8a8d490bb934dd955c32e00440c27f395fc1f7b0
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-08 00:06:34 +0530

    Add both normal and recursive method

--- src/com/integer/challenge/Factorial.java
+++ src/com/integer/challenge/Factorial.java
@@ -1,5 +1,25 @@
 package com.integer.challenge;
 
 public class Factorial {
+	
+	public static void main(String[] args) {
+		System.out.println(factorial(5));
+		System.out.println(factorialRecursive(5));
+	}
+	
+	static long factorial(int number) {
+		long factorial = 1;
+		for (int i = 1; i <= number; i++) {
+			factorial *= i;
+		}
+		return factorial;
+	}
+	
+	static long factorialRecursive(int number) {
+		if(number <= 1) {
+			return 1;
+		}
+		return number * factorial(number - 1);
+	}
 
 }
