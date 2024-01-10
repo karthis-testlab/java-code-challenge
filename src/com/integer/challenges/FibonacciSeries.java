ab55cffa9e4d84e147fa59c15363d9ff2d504e57
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-11 00:57:37 +0530

    11/01/2024:12:57:00AM

--- src/com/integer/challenges/FibonacciSeries.java
+++ src/com/integer/challenges/FibonacciSeries.java
@@ -2,8 +2,30 @@ package com.integer.challenges;
 
 public class FibonacciSeries {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		fibonacciSeriesForLoop(10);
+		fibonacciSeriesWhileLoop(10);
+	}
 
+	static void fibonacciSeriesForLoop(int number) {
+		int first = 0, second = 1;
+		for (int i = 0; i < number; i++) {
+			System.out.print(first+ " ");
+			int sum = first + second;
+			first = second;
+			second = sum;
+		}
+	}
+	
+	static void fibonacciSeriesWhileLoop(int number) {
+		int first = 0, second = 1, i = 0;
+		while(i < number) {
+			System.out.print(first+ " ");
+			int sum = first + second;
+			first = second;
+			second = sum;
+			i++;
+		}
 	}
 
 }
