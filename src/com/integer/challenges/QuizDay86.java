68045895cec12efc40358f570e048321d9315cf8
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-12 01:13:10 +0530

    12/01/2024:01:13:00AM

---
+++ src/com/integer/challenges/QuizDay86.java
@@ -0,0 +1,11 @@
+package com.integer.challenges;
+
+public class QuizDay86 {
+
+	public static void main(String[] args) {
+		int x = 5;
+	    int result = x++ + ++x * 2 + x-- - --x;
+	    System.out.println(result);
+	}
+
+}
