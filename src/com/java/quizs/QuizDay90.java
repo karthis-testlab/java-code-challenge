8750e48771e07b4ddb933c58791b1a02023854af
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-23 01:39:37 +0530

    23/01/2024:01:39:00AM

---
+++ src/com/java/quizs/QuizDay90.java
@@ -0,0 +1,13 @@
+package com.java.quizs;
+
+public class QuizDay90 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 0, j = n - 1; i < n; i++, j--) {
+			System.out.print(i + "" + j + " ");
+		}
+
+	}
+
+}
