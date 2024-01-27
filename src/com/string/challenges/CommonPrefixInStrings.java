3cef9eff602623ba3e5d095d9c5b53a1e2af3dcb
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-28 00:59:07 +0530

    28/01/2024:12:59:00AM

--- src/com/string/challenges/CommonPrefixInStrings.java
+++ src/com/string/challenges/CommonPrefixInStrings.java
@@ -4,16 +4,18 @@ public class CommonPrefixInStrings {
 
 	public static void main(String[] args) {
 		
-		String[] strings = {"flower", "flow"};
+		String[] strings = {"flower", "flight"};
 		String prefix = "";
 		
 		int minLength = Math.min(strings[0].length(), strings[1].length());
 		for (int i = 0; i < minLength; i++) {
 			if(strings[0].charAt(i) != strings[1].charAt(i)) {
-				System.out.println(strings[0].charAt(i));
+				prefix += strings[0].substring(0, i);
 				break;
 			}
 		}
+		
+		System.out.println(prefix);
 
 	}
 
