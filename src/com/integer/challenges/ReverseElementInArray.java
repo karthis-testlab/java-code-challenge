fa75bf8f1bfb2efd55c68866d66021c8e1e29739
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-30 01:26:00 +0530

    30/01/2024:01:25:00AM

--- src/com/integer/challenges/ReverseElementInArray.java
+++ src/com/integer/challenges/ReverseElementInArray.java
@@ -1,6 +1,8 @@
 package com.integer.challenges;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ReverseElementInArray {
 
@@ -20,7 +22,21 @@ public class ReverseElementInArray {
 		}		
 		
 		System.out.println("Reversed Array: "+Arrays.toString(numbers));
+		System.out.println("Reversed List: "+ getReversed(Arrays.asList(7, 17, 13, 19, 5)));
+		
 
+	}
+	
+	static List<Integer> getReversed(List<Integer> source) {
+		
+		ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+
+        for(int i = source.size() - 1; i >=0; i--) {
+            myNumbers.add(source.get(i));
+        }
+
+        return myNumbers;
+		
 	}
 
 }
