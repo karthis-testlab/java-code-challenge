aa62eadbb5944fe20b1f454b2560ae9a983bae73
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-31 00:34:54 +0530

    31/01/2024:12:34:00AM

--- src/com/java/read/files/ReadTextFile.java
+++ src/com/java/read/files/ReadTextFile.java
@@ -1,12 +1,24 @@
 package com.java.read.files;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ReadTextFile {
 
 	public static void main(String[] args) {
 		
 		File textFile = new File("./textfile.txt");
+		try {
+			Scanner scanner = new Scanner(textFile);
+			while(scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+			    System.out.println(line);
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 
 	}
