ba58b5437ebe3cc3dae357bbfa6068983fa25607
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-02-06 00:04:46 +0530

    06/02/2024:12:04:00AM

---
+++ src/com/java/quizs/QuizDay69.java
@@ -0,0 +1,16 @@
+package com.java.quizs;
+
+public class QuizDay69 {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Hello";
+		String str2 = "World";
+		int num = 42;
+		
+		System.out.println(str1 + str2 + num);
+		
+
+	}
+
+}
---
+++ src/com/java/quizs/QuizDay70.java
@@ -0,0 +1,12 @@
+package com.java.quizs;
+
+public class QuizDay70 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello");
+		System.out.println("Guys!");
+
+	}
+
+}
