d860183e7180f2d3f3f14e3db07b2cb7e7d7e57f
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-02-08 01:19:58 +0530

    08/02/2024:01:19:00AM

--- src/com/java/read/files/CsvFileHandler.java
+++ src/com/java/read/files/CsvFileHandler.java
@@ -1,24 +1,62 @@
 package com.java.read.files;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CsvFileHandler {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		String fileFolder = System.getProperty("user.dir")+"/data.csv";
-		
-		FileWriter csvWriter = new FileWriter(fileFolder);
-		
-		csvWriter.append("baseUrl, testUrl, threshold, status \n");
-		csvWriter.append("http://localhost:8080/indexA.html, http://localhost:8080/indexA.html, 0.01, NA \n");
-		
-		csvWriter.close();
-		
-		System.out.println("Successfully create csv file in the following folder "+fileFolder);
-	
+		writeFile();
+		readFile();
 
+	}
+
+	public static void writeFile() {
+		String fileFolder = System.getProperty("user.dir") + "/data.csv";
+
+		FileWriter csvWriter;
+
+		try {
+			csvWriter = new FileWriter(fileFolder);
+
+			csvWriter.append("baseUrl, testUrl, threshold, status \n");
+			csvWriter.append("http://localhost:8080/indexA.html, http://localhost:8080/indexA.html, 0.01, NA \n");
+
+			csvWriter.close();
+
+			System.out.println("Successfully create csv file in the following folder " + fileFolder);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void readFile() {
+
+		String fileFolder = System.getProperty("user.dir") + "/data.csv";
+
+		Scanner scanner;
+
+		try {
+			scanner = new Scanner(new File(fileFolder));
+
+			scanner.useDelimiter(",");
+
+			while (scanner.hasNext()) {
+				System.out.println(scanner.nextLine() + "\t");
+			}
+
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 
 }
