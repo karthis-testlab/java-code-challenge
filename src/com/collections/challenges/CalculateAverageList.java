dfc9a9ea48a9e1420d3bacdabffda5b2113eee36
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-02-09 00:55:52 +0530

    09/02/2024:12:55:00AM

--- src/com/collections/challenges/CalculateAverageList.java
+++ src/com/collections/challenges/CalculateAverageList.java
@@ -6,17 +6,25 @@ import java.util.List;
 public class CalculateAverageList {
 
 	public static void main(String[] args) {
+	
+		System.out.println(getAverage(Arrays.asList(2, 5, 20, 30, 56)));
+		System.out.println(getAverage(Arrays.asList(-20, 30, -55)));
 		
-		List<Integer> numbers = Arrays.asList(2, 5, 20, 30, 56);
-		double sum = 0;
-		
-		for (int i = 0; i < numbers.size(); i++) {
-			sum += numbers.get(i);
-		}
-		
-		System.out.println(sum/numbers.size());
+		System.out.println(getAverageJava8(Arrays.asList(2, 5, 20, 30, 56)));
+		System.out.println(getAverageJava8(Arrays.asList(-20, 30, -55)));
 
 	}
 	
+	static double getAverage(List<Integer> source) {
+		double sum = 0;
+		for (int i = 0; i < source.size(); i++) {
+			sum += source.get(i);
+		}
+		return sum/source.size();
+	}
+	
+	static double getAverageJava8(List<Integer> source) {		
+		return source.stream().reduce(0, Integer::sum) / (double) source.size();
+	}
 
 }
