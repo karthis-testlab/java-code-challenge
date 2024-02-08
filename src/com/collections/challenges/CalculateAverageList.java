25be57c53f1dda7284b92ed968d365e3cca6360b
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-02-09 00:19:35 +0530

    09/02/2024:12:19:00AM

--- src/com/collections/challenges/CalculateAverageList.java
+++ src/com/collections/challenges/CalculateAverageList.java
@@ -1,11 +1,22 @@
 package com.collections.challenges;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CalculateAverageList {
 
 	public static void main(String[] args) {
 		
+		List<Integer> numbers = Arrays.asList(2, 5, 20, 30, 56);
+		double sum = 0;
 		
+		for (int i = 0; i < numbers.size(); i++) {
+			sum += numbers.get(i);
+		}
+		
+		System.out.println(sum/numbers.size());
 
 	}
+	
 
 }
