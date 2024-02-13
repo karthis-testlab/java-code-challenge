925619a2634b302cc60ddf44d7e285c18133efee
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-02-14 01:11:57 +0530

    14/02/2024:01:11:00AM

---
+++ src/com/java/quizs/QuizDay58.java
@@ -0,0 +1,24 @@
+package com.java.quizs;
+
+public class QuizDay58 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		try {
+			
+			System.out.println(1);
+			
+			int i = Integer.parseInt("ABC");
+			
+			System.out.println(2);
+			
+		} catch (NumberFormatException nfe) {
+			System.out.println(3);
+		} catch (Exception e) {
+			System.out.println(4);
+		}
+
+	}
+
+}
