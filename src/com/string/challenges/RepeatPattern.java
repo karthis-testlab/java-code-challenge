2afa4b32cb5aeb161f502a657899aac370aca6a7
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-02-23 01:01:16 +0530

    23/02/2024:01:01:00AM

---
+++ src/com/string/challenges/RepeatPattern.java
@@ -0,0 +1,14 @@
+package com.string.challenges;
+
+public class RepeatPattern {
+
+	public static void main(String[] args) {
+		
+		String pattern = " $ ";
+		String repeatPattern = pattern.repeat(3);
+		System.out.println("Original Pattern: "+pattern);
+		System.out.println("Repeat Pattern: "+repeatPattern);
+
+	}
+
+}
