c0ea01173796d7680d717563b674583e2c7116d2
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-03-01 01:04:05 +0530

    01/03/2024:01:04:00AM

---
+++ src/com/string/challenges/PrintCurrentJavaVersionInSystem.java
@@ -0,0 +1,12 @@
+package com.string.challenges;
+
+public class PrintCurrentJavaVersionInSystem {
+
+	public static void main(String[] args) {
+		
+		String java_version = System.getProperty("java.version");	
+		System.out.println("Java Version in the system -> "+java_version+"v.");
+
+	}
+
+}
