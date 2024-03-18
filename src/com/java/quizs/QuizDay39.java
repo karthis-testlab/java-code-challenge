e597b68d98388b90971205afd4e0d87aeebd1c37
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-03-19 02:19:04 +0530

    19/03/2024:02:18AM

---
+++ src/com/java/quizs/QuizDay39.java
@@ -0,0 +1,19 @@
+package com.java.quizs;
+
+public class QuizDay39 {
+
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+
+		String str = null;
+
+		System.out.println(str.length());
+		
+		//Exception in thread "main" java.lang.NullPointerException
+		//will throw when we call String predefined method
+		//with String variable as null value.
+		
+
+	}
+
+}
