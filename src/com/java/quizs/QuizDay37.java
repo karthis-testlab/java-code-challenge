f28b59098fa8e87327aaf9cd2d80e57e7900dc4e
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-03-21 00:08:56 +0530

    21/03/2024:12:08AM

---
+++ src/com/java/quizs/QuizDay37.java
@@ -0,0 +1,19 @@
+package com.java.quizs;
+
+public class QuizDay37 {
+
+	public static void main(String[] args) {
+		
+		Integer num1 = 127;
+		Integer num2 = 127;
+		
+		System.out.println(num1 == num2);
+		
+		Integer num3 = 128;
+		Integer num4 = 128;
+		
+		System.out.println(num3 == num4);
+
+	}
+
+}
