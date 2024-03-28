7c29a9eb60df813d2f5e7903ee3e1764fa9d8aa4
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-03-29 02:45:05 +0530

    29/03/2024:02:45AM

--- src/com/java/file/handlers/LoggerTest.java
+++ src/com/java/file/handlers/LoggerTest.java
@@ -1,10 +1,12 @@
 package com.java.file.handlers;
 
+import static com.java.file.handlers.Logger.*;
+
 public class LoggerTest {
 
 	public static void main(String[] args) {
-		Logger.info("Hi! info Logger.");
-		Logger.fail("Hi! failed Logger.");
+		info("Hi! info Logger.");
+		fail("Hi! failed Logger.");
 	}
 
 }
