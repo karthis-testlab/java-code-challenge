244adf299257ae7ccc570db84a170fc46c4407b4
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-04-11 00:39:34 +0530

    11/04/2024:12:39AM

---
+++ src/com/java/quizs/QuizDay32.java
@@ -0,0 +1,14 @@
+package com.java.quizs;
+
+public class QuizDay32 {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		int b = a;
+		b = a++ + b;
+		System.out.println(b);
+
+	}
+
+}
