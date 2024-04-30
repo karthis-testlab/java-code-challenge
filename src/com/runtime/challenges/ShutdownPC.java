d8fd87a8f5a81962bfcd07f930e044f9c2aaa204
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-04-30 10:56:34 +0530

    30/04/2024:10:56AM

--- src/com/runtime/challenges/ShutdownPC.java
+++ src/com/runtime/challenges/ShutdownPC.java
@@ -3,12 +3,23 @@ package com.runtime.challenges;
 public class ShutdownPC {
 
 	public static void main(String[] args) {
-		try {			
-			// Shutdown the PC
-			Runtime.getRuntime().exec(new String[] {"shutdown /s /t 1"});			
+		String[] shutdownCommand;
+		String operatingSystem = System.getProperty("os.name");
+		try {
+			// Shutdown the PC based on system operating system
+			if("Linux".equals(operatingSystem) || "Mac OS X".equals(operatingSystem)) {
+				shutdownCommand = new String[] {"shutdown -h now"};
+			} else if (operatingSystem.contains("Windows")) {
+				// This will work on any version of windows including version 11 
+				shutdownCommand = new String[] {"shutdown.exe -s -t 0"};
+			} else {
+		        throw new RuntimeException("Unsupported operating system.");
+		    }
+			Runtime.getRuntime().exec(shutdownCommand);
+		    System.exit(0);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}		
+		}
 
 	}
 
