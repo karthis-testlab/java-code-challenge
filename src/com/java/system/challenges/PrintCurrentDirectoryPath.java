2b549603d401ff4a5a9c5f1daf2d33af7fd18323
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-05-01 23:58:02 +0530

    01/05/2024:11:57AM

---
+++ src/com/java/system/challenges/PrintCurrentDirectoryPath.java
@@ -0,0 +1,9 @@
+package com.java.system.challenges;
+
+public class PrintCurrentDirectoryPath {
+
+	public static void main(String[] args) {
+		System.out.println("Current Directory Path is: "+System.getProperty("user.dir"));		
+	}
+
+}
---
+++ src/com/java/system/challenges/PrintCurrentSystemOsName.java
@@ -0,0 +1,9 @@
+package com.java.system.challenges;
+
+public class PrintCurrentSystemOsName {
+
+	public static void main(String[] args) {
+		System.out.println("Current System Operating System Name is: "+System.getProperty("os.name"));
+	}
+
+}
