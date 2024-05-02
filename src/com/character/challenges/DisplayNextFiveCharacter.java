968ab8267d1c0b49992cb9aa74c879f6dbc61ef8
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-05-03 00:03:30 +0530

    03/05/2024:12:03AM

--- src/com/character/challenges/DisplayNextFiveCharacter.java
+++ src/com/character/challenges/DisplayNextFiveCharacter.java
@@ -1,22 +1,42 @@
 package com.character.challenges;
 
+import java.util.Scanner;
+
 public class DisplayNextFiveCharacter {
 	
-	
+   /*
+    *  Write a java program to enter a letter and display next five letter
+    *  Input - capital (W)
+    *  Output - XYZab
+    *  
+    *   Or
+    *   
+    *  Input - small (x)
+    *  Output - yzABC 
+    * 
+    */
 
 	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter an Alphabet: ");
+		char ch = in.next().charAt(0);
+		
 		DisplayNextFiveCharacter displayNextFiveCharacter = new DisplayNextFiveCharacter();
-		displayNextFiveCharacter.display('A');
+		displayNextFiveCharacter.display(ch);
+		
+		in.close();
 	}
 
 	void display(char ch) {
 		for (int i = 0; i < 5; i++) {
+			ch++;
 			if (ch > 'z') {
 				ch = 'A';
 			} else if (ch > 'Z' && ch < 'a') {
 				ch = 'a';
 			}
-			System.out.print(ch++);
+			System.out.print(ch);
 		}
 	}
 
