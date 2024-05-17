b6d1d2aa4c7a0006d901f20296b261203efb9b99
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-05-18 00:39:03 +0530

    18/05/2024:12:38AM

---
+++ src/com/java/quizs/QuizDay28.java
@@ -0,0 +1,18 @@
+package com.java.quizs;
+
+import java.util.ArrayList;
+
+public class QuizDay28 {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		
+		ArrayList obj = new ArrayList();  
+		obj.add("A");	
+		obj.add("B");
+		obj.add(1, "C");
+		obj.add("D");
+		System.out.println(obj);
+	}
+
+}
