9ce82871ea895d35c7c593cf202b637d9380f1fd
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-05-22 00:00:16 +0530

    22/05/2024:12:00AM

---
+++ src/com/java/quizs/QuizDay23.java
@@ -0,0 +1,14 @@
+package com.java.quizs;
+
+public class QuizDay23 {
+
+	public static void main(String[] args) {
+		
+		String str = " ";
+		
+		System.out.println(str.isBlank());
+		System.out.println(str.isBlank());
+
+	}
+
+}
