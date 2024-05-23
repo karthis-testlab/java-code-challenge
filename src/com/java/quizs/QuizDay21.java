bc6e8aa94855548e692498af4580c6d34352501a
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-05-24 00:12:29 +0530

    24/05/2024:12:12AM

---
+++ src/com/java/quizs/QuizDay21.java
@@ -0,0 +1,14 @@
+package com.java.quizs;
+
+public class QuizDay21 {
+
+	public static void main(String[] args) {
+		
+		String str1 = "DaIlY JaVa QuIz";
+		String str2 = "DailyJavaQuiz";		
+		
+		System.out.println(str1.trim().equalsIgnoreCase(str2));
+
+	}
+
+}
