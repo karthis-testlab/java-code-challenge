0a5e6b965b11711941e4d96c51fd0cd55b6ffc2a
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-06-10 01:30:56 +0530

    10/06/2024:01:30AM

---
+++ src/com/java/quizs/QuizDay14.java
@@ -0,0 +1,12 @@
+package com.java.quizs;
+
+public class QuizDay14 {
+
+	public static void main(String[] args) {
+		int arr[] = {'a', 'b', 'c', 'd'};
+		for (int i = 0; i < 5; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
