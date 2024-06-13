6b4bf7a6b946dd3082b8cfe2eb977f4fe2cc05e5
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-06-14 02:39:08 +0530

    14/06/2024:02:39AM

---
+++ src/com/java/quizs/QuizDay11.java
@@ -0,0 +1,18 @@
+package com.java.quizs;
+
+public class QuizDay11 {
+
+	public static void main(String[] args) {
+		
+		Integer num1 = 100;
+		Integer num2 = 100;
+		
+		Integer num3 = 1000;
+		Integer num4 = 1000;
+		
+		System.out.println((num1 == num2)+" "+num1.equals(num2)); 
+		System.out.println((num3 == num4)+" "+num3.equals(num4));
+
+	}
+
+}
