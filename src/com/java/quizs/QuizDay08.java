6857f9afff96c233e5429559eb0416c3458679c3
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-06-17 01:11:00 +0530

    17/06/2024:01:10AM

---
+++ src/com/java/quizs/QuizDay08.java
@@ -0,0 +1,12 @@
+package com.java.quizs;
+
+public class QuizDay08 {
+
+	public static void main(String[] args) {
+		
+		String number = "1";
+		System.out.println(number.concat("2").concat("3")); 
+
+	}
+
+}
