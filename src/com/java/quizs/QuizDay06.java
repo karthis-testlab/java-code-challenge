8b9dd4a3d15029d59d5b79ca599f41bdbcd4b098
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-06-19 01:27:00 +0530

    19/06/2024:01:26AM

---
+++ src/com/java/quizs/QuizDay06.java
@@ -0,0 +1,15 @@
+package com.java.quizs;
+
+public class QuizDay06 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Inside Main Block!");
+
+	}
+	
+	static {
+		System.out.println("Inside Static Block!");
+	}
+
+}
