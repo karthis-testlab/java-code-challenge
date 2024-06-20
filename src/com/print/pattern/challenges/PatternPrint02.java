2d7681ad603ac9c9823652c1e35c318527c6c82e
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-06-21 02:49:54 +0530

    21/06/2024:02:49AM

--- src/com/print/pattern/challenges/PatternPrint02.java
+++ src/com/print/pattern/challenges/PatternPrint02.java
@@ -1,11 +1,11 @@
 package com.print.pattern.challenges;
 
-public class PatternPrint01 {
+public class PatternPrint02 {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < i; j++) {
-				System.out.print("*");
+				System.out.print(j);
 			}
 			System.out.println();
 		}
