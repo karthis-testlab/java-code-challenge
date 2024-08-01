9471a52220bd1278d035b1119f4ba3ec851e18c5
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-01 21:36:50 +0530

    01/08/2024:09:37PM

---
+++ src/com/java/quizs/QuizDay02.java
@@ -0,0 +1,13 @@
+package com.java.quizs;
+
+public class QuizDay02 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		if(4.0 == 1+1+1+1) System.out.println("True");
+		else System.out.println("False"); 
+
+	}
+
+}
