6ab5d8a8652741e49132da8da112116d49021ebf
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-08 01:10:13 +0530

    08/08/2024:01:10AM

---
+++ src/com/java/quizs/QuizDay92.java
@@ -0,0 +1,13 @@
+package com.java.quizs;
+
+public class QuizDay92 {
+
+	@SuppressWarnings("removal")
+	public static void main(String[] args) {
+		
+		Object x[]	= new String[3];
+		x[0] = new Integer(0);
+
+	}
+
+}
