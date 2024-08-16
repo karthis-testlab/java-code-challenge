4e1093f74bbcad6ca0a4601209c40dd9e13092df
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-17 01:30:48 +0530

    17/08/2024:01:30AM

---
+++ src/com/java/quizs/QuizDay94.java
@@ -0,0 +1,13 @@
+package com.java.quizs;
+
+public class QuizDay94 {
+
+	public static void main(String[] args) {
+		
+		String string = "abcd";
+		string = string.toUpperCase().substring(1, 3);
+		System.out.println(string);
+
+	}
+
+}
