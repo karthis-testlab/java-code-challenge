352a317956678b610ed830c642195839fc207f3b
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-23 00:26:11 +0530

    23/08/2024:12:26:00AM

---
+++ src/com/java/quizs/QuizDay96.java
@@ -0,0 +1,12 @@
+package com.java.quizs;
+
+public class QuizDay96 {
+	
+	static String name = "Karthikeyan";
+	static int number = 94;
+
+	public static void main(String[] args) {		
+		System.out.println(name+"\n"+number);
+	}
+
+}
