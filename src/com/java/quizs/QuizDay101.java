33171d35bd92bcacf30575f23f687f933bb21177
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-08-30 01:38:49 +0530

    30/08/2024:01:38:00AM

---
+++ src/com/java/quizs/QuizDay101.java
@@ -0,0 +1,12 @@
+package com.java.quizs;
+
+public class QuizDay101 {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = s1.concat(" World");
+		s2.concat("!");
+		System.out.println(s2);
+	}
+
+}
