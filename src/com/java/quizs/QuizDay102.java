7aff952ca414fbea6d7b1976e40ea3726e5a6e29
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-11-30 00:42:17 +0530

    30/11/2024:12:42:00AM

--- src/com/java/quizs/QuizDay102.java
+++ src/com/java/quizs/QuizDay102.java
@@ -2,8 +2,7 @@ package com.java.quizs;
 
 public class QuizDay102 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 
 	}
 
