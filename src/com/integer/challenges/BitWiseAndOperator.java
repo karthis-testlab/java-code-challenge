8969a1c5ffa5e24302cdd7b4e7f9942a5d93b656
Karthikeyan Rajendran <69006157+karthis-testlab@users.noreply.github.com>
2025-03-21 21:53:59 +0100

    Update BitWiseAndOperator.java

--- src/com/integer/challenges/BitWiseAndOperator.java
+++ src/com/integer/challenges/BitWiseAndOperator.java
@@ -2,7 +2,7 @@ package com.integer.challenges;
 
 public class BitWiseAndOperator {
 
-	//
+	// Code for Bit Wise & Operator
 	public static void main(String[] args) {	
 		int x = 4;
 		System.out.println(x & -x);
